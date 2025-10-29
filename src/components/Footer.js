9dfae142007850f9e76992de56c411213c13cb30
rrroy <hly5640@gmail.com>
2025-10-29 11:34:44 +1100

    Update Footer logo to match Header
    - Replace Footer logo from PNG to SVG format
    - Use same Thriving Capital.svg file as Header for consistency

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import logo from "../assets/Thriving Capital.png";
+import logo from "../assets/Thriving Capital.svg";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
