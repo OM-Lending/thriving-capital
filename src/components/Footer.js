48bcd5662a944eafe63565912e0028e8554108ef
OM-Sherry <sherry@omlending.com.au>
2026-05-04 15:36:13 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -224,9 +224,9 @@ const Footer = () => {
                 </a>
               </div>
               <div className="footer-contact-item">
-                <span className="footer-contact-icon">📍</span>
+                <span className="footer-contact-icon1">📍</span>
                 <span>Level 4, 162 Macquarie St, Hobart TAS 7000</span>
-                <span className="footer-contact-icon">📍</span>
+                <span className="footer-contact-icon2">📍</span>
                 <span>Level 10, 88 Tribune Street, South Brisbane QLD 4101</span>
               </div>
             </Col>
